ab1355debf2b89a4144bd92d738f0c121011ee65
Eduardo gonçalves <eduardogoncalves3692@gmail.com>
2024-01-11 20:03:56 -0300

    add style pagination home

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,12 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-
   h1 {
     font-size: 1.4rem;
     text-align: center;
     color: lightgray;
     margin-top: 0.8rem;
+  }
+
+  .paginationComponent {
+    width: 22rem;
+    margin: 1.4rem auto 0;
+    padding: 0.6rem 1.2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .boxNumberPage {
+      font-size: 1.2rem;
+      font-weight: 700;
+    }
+
+    button {
+      background: ${({ theme }) => theme.colors.danger};
+      color: ${({ theme }) => theme.colors.light};
+      font-size: 1rem;
+      font-weight: 700;
+      padding: 0.6rem 1.2rem;
+      border-radius: 0.8rem;
+      box-shadow: 1px 1px 2px 0px #0008;
+      text-shadow: 1px 1px 2px 0px #0004;
+
+      &:disabled {
+        background: lightgray;
+      }
+
+      &:not(:disabled):hover{
+        filter: brightness(1.05);
+      }
+    }
   }
 
   .gridCards {
